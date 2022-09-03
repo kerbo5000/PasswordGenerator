88353fb134e6329052266ec372f1bc87239b15c2
kerbo5000 <kerbysd@hotmail.com>
2022-09-03 18:25:29 -0400

    change url

--- includes/extraComponents/functions.php
+++ includes/extraComponents/functions.php
@@ -63,8 +63,13 @@ function dec($data,$private_key) {
   return $plaintext;
 }
 function getHash($string,$index_key){
-    echo 'h26';
-    $index_key = base64_decode($index_key);
-    return bin2hex(sodium_crypto_pwhash(32,$string,$index_key,SODIUM_CRYPTO_PWHASH_OPSLIMIT_MODERATE,SODIUM_CRYPTO_PWHASH_MEMLIMIT_MODERATE));
+    
+    try {
+      echo 'h26';
+      $index_key = base64_decode($index_key);
+      return bin2hex(sodium_crypto_pwhash(32,$string,$index_key,SODIUM_CRYPTO_PWHASH_OPSLIMIT_MODERATE,SODIUM_CRYPTO_PWHASH_MEMLIMIT_MODERATE));
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    }
 	}
 ?>
