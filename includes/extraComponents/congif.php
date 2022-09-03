835bbf73d8ba9aca6a25ee51a68ae8213aaffe47
kerbo5000 <kerbysd@hotmail.com>
2022-09-03 14:53:29 -0400

    database connection error

--- includes/extraComponents/congif.php
+++ includes/extraComponents/congif.php
@@ -3,14 +3,15 @@
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $DB_HOST = $cleardb_url["host"]; //localhost
-$DB_NAME = "heroku_3fb36c838d9f1bd"; //password_storage
 $DB_USR = $cleardb_url["user"]; // root
 $DB_PWD = $cleardb_url["pass"];
+$DB_NAME = substr($cleardb_url["path"], 1);
+$DB_PORT = $cleardb_url["port"],
 // encryption/decryption key
 $private_key = "3sME4onR0Zbsybkeg8Ql4nLE5EAIfsjx20ybOV4N/5M="; //3sME4onR0Zbsybkeg8Ql4nLE5EAIfsjx20ybOV4N/5M=
 // hashing key
 $index_key = "CWjGRyEpefknC39hrTUnww=="; //CWjGRyEpefknC39hrTUnww==
-$pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME",$DB_USR,$DB_PWD);
+$pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;port=$DB_PORT",$DB_USR,$DB_PWD);
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 ?>
 
